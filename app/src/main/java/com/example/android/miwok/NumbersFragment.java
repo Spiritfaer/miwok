cb3523f14f3ae106b8abdd180e0d60a79a8507f1
Spiritfaer <30905488+Spiritfaer@users.noreply.github.com>
2020-11-28 10:58:10 +0200

    Add fragment logic for NumbersActivity

---
+++ app/src/main/java/com/example/android/miwok/NumbersFragment.java
@@ -0,0 +1,136 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * create an instance of this fragment.
+ */
+public class NumbersFragment extends Fragment {
+
+    //Start of NumberActivity section
+    private MediaPlayer mPlayer;
+    private AudioManager audioManager;
+    private AudioManager.OnAudioFocusChangeListener afChangeListener;
+
+    private final ArrayList<Word> words;
+    {
+        words = new ArrayList<>();
+        words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
+        words.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
+        words.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
+        words.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
+        words.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
+        words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
+        words.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
+        words.add(new Word("nine", "wo'e", R.drawable.number_nine, R.raw.number_nine));
+        words.add(new Word("ten", "na'aacha", R.drawable.number_ten, R.raw.number_ten));
+        Log.v("NumberActivity", "init words in ArrayList");
+    }
+
+    private void initAudioManager() {
+        audioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+        afChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+            @Override
+            public void onAudioFocusChange(int focusChange) {
+                if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK ||
+                        focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
+                    mPlayer.pause();
+                    mPlayer.seekTo(0);
+                } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                    releaseMediaPlayer();
+                } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                    mPlayer.start();
+                }
+            }
+        };
+    }
+
+    private void initItemListenerForPlayer(View rootView) {
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                releaseMediaPlayer();
+                int requestResult = audioManager.requestAudioFocus(afChangeListener,
+                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if (requestResult == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mPlayer = MediaPlayer.create(getActivity(), words.get(position).getAudio());
+                    mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            releaseMediaPlayer();
+                        }
+                    });
+                    Log.v("FocusRequest", "focus request result :" + requestResult);
+                    mPlayer.start();
+                }
+            }
+        });
+    }
+
+    private void initAdapter(View rootView) {
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+        WordsAdapter itemsAdapter = new WordsAdapter(getActivity(), R.layout.list_item, words, R.color.category_numbers);
+        listView.setAdapter(itemsAdapter);
+    }
+
+    private void releaseMediaPlayer() {
+        if (audioManager != null && afChangeListener != null) {
+            audioManager.abandonAudioFocus(afChangeListener);
+        }
+        if (mPlayer != null) {
+            if (mPlayer.isPlaying()) {
+                mPlayer.stop();
+            }
+            mPlayer.release();
+        }
+        mPlayer = null;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // When the activity is stopped, release the media player resources because we won't
+        // be playing any more sounds.
+        releaseMediaPlayer();
+    }
+
+//End of NumberActivity section
+
+    public NumbersFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+        initAdapter(rootView);
+        initItemListenerForPlayer(rootView);
+        initAudioManager();
+
+        return rootView;
+    }
+}
